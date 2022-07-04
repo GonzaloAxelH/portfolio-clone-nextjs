993c4e08b3336a5cefbba872fbc573846b0a566c
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-07-03 22:17:01 -0500

    d

--- pages/videos/index.js
+++ pages/videos/index.js
@@ -18,9 +18,7 @@ const TutorialVideos = ({ onToggleTheme }) => {
           modestbranding={1}
           src="https://www.youtube.com/embed/rj7DOPPTkQA?controls=0"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullscreen
         ></iframe>
       </div>
     </Layout>
