de20a11189a46a38790257e1bec30a9abe7fc466
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-07-09 18:17:41 -0500

    add test archive to server

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,5 +2,8 @@ export default (req, res) => {
   console.log(req);
   res.setHeader("Cache-control", "s-maxage=6000, stale-while-revalidate=30");
   res.setHeader("Content-type", "text/plain");
-  res.status(200).end("example from Nextjs !!");
+  res.status(200).json({
+    message: "Hola desde Nextjs",
+    anguloServo: "29",
+  });
 };
